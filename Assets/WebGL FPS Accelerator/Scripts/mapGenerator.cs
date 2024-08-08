ed8c3446b45cb1a32db4bc692231095499250783
Kanopuster <yusup.abdulyaparov@gmail.com>
2024-08-08 12:55:18 +0500

    более частый спавн префабов

--- Assets/WebGL FPS Accelerator/Scripts/mapGenerator.cs
+++ Assets/WebGL FPS Accelerator/Scripts/mapGenerator.cs
@@ -1,85 +1,92 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-// WFA 2.0
+
 namespace WFA
 {
-  public class mapGenerator : MonoBehaviour
-  {
-    private float spacing = 21f;
-
-    
-    public GameObject prefabSpawn;
-
-    [HideInInspector]
-    public int size_x = 7;
-    [HideInInspector]
-    public int size_z = 7;
-
-    private List<GameObject> prefabList = new List<GameObject>();
-
-    [HideInInspector]
-    public Slider Slider;
-
-    [HideInInspector]
-    public Transform parent;
-
-    private bool useColor = true;
-
-    public void Start()
+    public class mapGenerator : MonoBehaviour
     {
-      sliderEvent();
-    }
+        public float spacing = 21f;
 
-    public void sliderEvent()
-    {
-      size_x = (int)Slider.value;
-      size_z = (int)Slider.value;
+        public GameObject prefabSpawn;
+        
+        [HideInInspector]
+        public int size_x = 7;
+        
+        [HideInInspector]
+        public int size_z = 7;
 
-      generate();
-    }
+        private List<GameObject> prefabList = new List<GameObject>();
 
-    public void generate()
-    {
-      foreach (var item in prefabList)
-      {
-        DestroyImmediate(item);
-      }
+        [HideInInspector]
+        public Slider Slider;
 
-      prefabList.Clear();
+        [HideInInspector]
+        public Transform parent;
 
-      Vector3 pos = Vector3.zero;
+        public TextMeshProUGUI prefabCountText; // Add this line to declare a Text component
 
-      for (int x = 0; x < size_x; x++)
-      {
-        pos.x = x * spacing;
+        private bool useColor = true;
 
-        for (int z = 0; z < size_z; z++)
+        public void Start()
         {
-          pos.z = z * spacing;
-
-          GameObject go = Instantiate(prefabSpawn);
-          go.transform.position = pos;
-          go.transform.parent = parent;
-
-          prefabList.Add(go);
+            sliderEvent();
         }
-      }
 
-      if (useColor)
-      {
+        public void sliderEvent()
         {
-          for (int i = 0; i < prefabList.Count; i++)
-          {
-            Color Color1 = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+            size_x = (int)Slider.value;
+            size_z = (int)Slider.value;
 
-            List<Renderer> renderers = prefabList[i].GetComponentsInChildren<Renderer>().ToList();
-            renderers[0].material.color = Color1;
-            renderers[1].material.color = Color1;
-          }
+            generate();
+            UpdatePrefabCountText(); // Add this line to update the text when slider changes
         }
-      }
+
+        public void generate()
+        {
+            foreach (var item in prefabList)
+            {
+                DestroyImmediate(item);
+            }
+
+            prefabList.Clear();
+
+            Vector3 pos = Vector3.zero;
+
+            for (int x = 0; x < size_x; x++)
+            {
+                pos.x = x * spacing;
+
+                for (int z = 0; z < size_z; z++)
+                {
+                    pos.z = z * spacing;
+
+                    GameObject go = Instantiate(prefabSpawn);
+                    go.transform.position = pos;
+                    go.transform.parent = parent;
+
+                    prefabList.Add(go);
+                }
+            }
+
+            if (useColor)
+            {
+                for (int i = 0; i < prefabList.Count; i++)
+                {
+                    Color Color1 = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+
+                    List<Renderer> renderers = prefabList[i].GetComponentsInChildren<Renderer>().ToList();
+                    renderers[0].material.color = Color1;
+                    renderers[1].material.color = Color1;
+                }
+            }
+        }
+
+        private void UpdatePrefabCountText()
+        {
+            prefabCountText.text = $"Prefabs: {prefabList.Count * 12000}";
+        }
     }
-  }
 }
